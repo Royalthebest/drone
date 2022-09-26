e30fb764c7cf75b3577b7fd2fb3e5fb4ee098972
Royalthebest <sam901009@gmail.com>
2022-09-26 22:23:54 +0800

    qq

--- lab1/lab1_2.py
+++ lab1/lab1_2.py
@@ -9,23 +9,17 @@ for i in range(h*3-3):
         x,y = i/3, j/3
         lx = int(np.floor(x))
         ly = int(np.floor(y))
-        # print(lx,ly)
-        tmp1 = [0,0,0]
-        tmp2 = [0,0,0]
+        dx = x-lx
+        dy = y-ly
+        a,b,c,d = img[lx][ly], img[lx+1][ly], img[lx][ly+1], img[lx+1][ly+1]
+        if(lx+1>=h):
+            b=[0,0,0]
+            d=[0,0,0]
+        if(ly+1>=w):
+            c=[0,0,0]
+            d=[0,0,0]
         for k in range(3):
-            if(lx+1>=h):
-                tmp1[k] = img[lx][ly][k]*(1-x+lx)
-            else:
-                tmp1[k] = img[lx][ly][k]*(1-x+lx)+img[lx+1][ly][k]*(x-lx)
-
-            if(ly+1>=w):
-                tmp2[k] = 0
-            elif(lx+1>=h):
-                tmp2[k] = img[lx][ly+1][k]*(1-x+lx)
-            else:
-                tmp2[k] = img[lx][ly+1][k]*(1-x+lx)+img[lx+1][ly+1][k]*(x-lx)
-        for k in range(3):
-            prod[i][j][k] = tmp1[k]*(1-y+ly) + tmp2[k]*(y-ly)
+            prod[i][j][k] = (1-dy)*(a[k]*(1-dx)+b[k]*dx) + dy*(c[k]*(1-dx)+d[k]*dx)
 
 cv2.imwrite('test_bi.jpg', prod)
 cv2.waitKey()
